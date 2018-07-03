c8cd969edf6f9d6c7bcd14bb266aee3497e00f36
bigcrushes <36911839+bigcrushes@users.noreply.github.com>
2018-07-03 18:45:55 +0800

    Comment box

--- script.js
+++ script.js
@@ -10,7 +10,10 @@ $(document).ready(function () {
 	});
 
 	
-
+	$.get("https://wd.comsci.club/api/kv.php?key=myprivatecommentsection", function(data) {
+			$("#comments-box").html(data);
+		});
+	
 	$("#submitcomment").click(function() {
 		var currentcomments = $("#comments-box").html();
 		var newcomments = currentcomments + "<br>" + "<b>" + $("#commentName").val() + "</b>: " + $("#comment").val();
